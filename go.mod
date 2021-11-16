6a0e79f9a85370cf3e0828fe6d6c416c3d7b67bb
gohe <2812782758@qq.com>
2021-11-16 14:39:45 +0800

    add  rest api

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/gouez/gg-seq
 
 go 1.17
 
-require github.com/go-sql-driver/mysql v1.6.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gouez/gg-seq/comm v0.0.0-20211116062045-f6290acc5ae0 // indirect
+)
--- server/controller/ctl.go
+++ server/controller/ctl.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 	"strconv"
 
@@ -13,6 +15,19 @@ func GetHandlers(idGeneratorFactory comm.IdGeneratorFactory) map[string]http.Han
 	handlers["/get"] = func(rw http.ResponseWriter, r *http.Request) {
 		bizType := r.URL.Query().Get("bizType")
 		rw.Write([]byte(strconv.FormatUint(idGeneratorFactory.GetIdGenerator(bizType).GetId(), 10)))
+	}
+
+	handlers["/get/batch"] = func(rw http.ResponseWriter, r *http.Request) {
+		values := r.URL.Query()
+		bizType := values.Get("bizType")
+		size := values.Get("size")
+		isize, _ := strconv.Atoi(size)
+		ids := idGeneratorFactory.GetIdGenerator(bizType).GetIds(isize)
+		v, err := json.Marshal(ids)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		rw.Write([]byte(v))
 	}
 	return handlers
 }
